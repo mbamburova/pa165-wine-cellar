ee8d67e3651d35f8e151e48c045f4940940c31d9
Michaela Bamburová <433732@mail.muni.cz>
2016-10-30 23:42:08 +0100

    test

--- wine-cellar-persistence/src/test/java/cz/muni/fi/pa165/dao/PriceDaoTest.java
+++ wine-cellar-persistence/src/test/java/cz/muni/fi/pa165/dao/PriceDaoTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
 @Transactional
 public class PriceDaoTest extends AbstractTestNGSpringContextTests {
+
     
  /*   @Autowired
     private PriceDao priceDao;
