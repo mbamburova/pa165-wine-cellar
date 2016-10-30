e4a50691d740cf8167a1c03809b7ec96a5bb6e2b
marekscholtz <marek.scholtz@gmail.com>
2016-10-30 21:32:58 +0100

    Entity (Price) + DTO, DAO (WineList, MarketingEvent)

--- wine-cellar-persistence/src/main/java/cz/muni/fi/pa165/entity/Price.java
+++ wine-cellar-persistence/src/main/java/cz/muni/fi/pa165/entity/Price.java
@@ -25,4 +25,43 @@ public class Price {
     @NotNull
     private Currency currency;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Price price = (Price) o;
+
+        return id.equals(price.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
