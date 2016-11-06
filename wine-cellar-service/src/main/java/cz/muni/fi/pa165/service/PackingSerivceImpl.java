f574d2d0179e872ada587b24098f3df3fdc04773
marekscholtz <marek.scholtz@gmail.com>
2016-11-06 21:35:35 +0100

    PackingService added

--- wine-cellar-service/src/main/java/cz/muni/fi/pa165/service/PackingSerivceImpl.java
+++ wine-cellar-service/src/main/java/cz/muni/fi/pa165/service/PackingSerivceImpl.java
@@ -1,8 +1,85 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.dao.PackingDao;
+import cz.muni.fi.pa165.dao.PriceDao;
+import cz.muni.fi.pa165.entity.Packing;
+import cz.muni.fi.pa165.entity.Price;
+import org.joda.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * @author MarekScholtz
  * @version 2016.11.06
  */
 public class PackingSerivceImpl implements PackingService {
+
+    @Autowired
+    private PackingDao packingDao;
+
+    @Autowired
+    private PriceDao priceDao;
+
+    @Override
+    public void create(Packing packing) {
+        packingDao.create(packing);
+    }
+
+    @Override
+    public Packing get(Long id) {
+        return packingDao.get(id);
+    }
+
+    @Override
+    public List<Packing> getAll() {
+        return packingDao.getAll();
+    }
+
+    @Override
+    public void update(Packing packing) {
+        packingDao.update(packing);
+    }
+
+    @Override
+    public void updateVolume(Packing packing, BigDecimal volume) {
+        packing.setVolume(volume);
+        packingDao.update(volume);
+    }
+
+    @Override
+    public void updateValidFrom(Packing packing, LocalDateTime validFrom) {
+        packing.setValidFrom(validFrom);
+        packingDao.update(volume);
+    }
+
+    @Override
+    public void updateValidTo(Packing packing, LocalDateTime validTo) {
+        packing.setValidTo(validTo);
+        packingDao.update(volume);
+    }
+
+    @Override
+    public void delete(Packing packing) {
+        for (Price price : packing.getPrices()) {
+            priceDao.delete(price);
+        }
+        packingDao.delete(packing);
+    }
+
+    @Override
+    public boolean isValid(Packing packing) {
+        return packing.getValidFrom().isBefore(LocalDateTime.now()) && packing.getValidTo().isAfter(LocalDateTime.now());
+    }
+
+    @Override
+    public void addPrice(Packing packing, Price price) {
+        packingDao.addPrice(packing, price);
+    }
+
+    @Override
+    public void removePrice(Packing packing, Price price) {
+        packingDao.removePrice(packing, price);
+    }
 }
--- wine-cellar-service/src/main/java/cz/muni/fi/pa165/service/PackingService.java
+++ wine-cellar-service/src/main/java/cz/muni/fi/pa165/service/PackingService.java
@@ -1,8 +1,33 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.entity.Packing;
+import cz.muni.fi.pa165.entity.Price;
+import org.joda.time.LocalDateTime;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * @author MarekScholtz
  * @version 2016.11.06
  */
 public interface PackingService {
+
+    void create(Packing packing);
+
+    Packing get(Long id);
+    List<Packing> getAll();
+
+    void update(Packing packing);
+    void updateVolume(Packing packing, BigDecimal volume);
+    void updateValidFrom(Packing packing, LocalDateTime validFrom);
+    void updateValidTo(Packing packing, LocalDateTime validTo);
+
+    void delete(Packing packing);
+
+    boolean isValid(Packing packing);
+
+    void addPrice(Packing packing, Price price);
+    void removePrice(Packing packing, Price price);
+
 }
