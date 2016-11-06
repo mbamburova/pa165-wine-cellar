fd054957c0c0d75df8c26d4e73d0c7ff9f5c5efa
Tomáš Gordian <xgordian@fi.muni.cz>
2016-11-06 22:06:59 +0100

    facade implementation start

--- wine-cellar-service/src/main/java/cz/muni/fi/pa165/facade/MarketingEventFacadeImpl.java
+++ wine-cellar-service/src/main/java/cz/muni/fi/pa165/facade/MarketingEventFacadeImpl.java
@@ -1,7 +1,55 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.MarketingEventDto;
+
+import java.util.List;
+
 /**
  * @author Tomas Gordian on 11/6/2016.
  */
-public class MarketingEventFacadeImpl {
+public class MarketingEventFacadeImpl implements MarketingEventFacade {
+    @Override
+    public void create(MarketingEventDto marketingEvent) {
+
+    }
+
+    @Override
+    public void delete(Long marketingEventId) {
+
+    }
+
+    @Override
+    public void update(Long marketingEventId) {
+
+    }
+
+    @Override
+    public void updateDescription(Long marketingEventId, String name) {
+
+    }
+
+    @Override
+    public List<MarketingEventDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public MarketingEventDto get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MarketingEventDto> findByDescription(String name) {
+        return null;
+    }
+
+    @Override
+    public MarketingEventDto addPrice(Long marketingEventId, Long priceId) {
+        return null;
+    }
+
+    @Override
+    public MarketingEventDto removePrice(Long marketingEventId, Long priceId) {
+        return null;
+    }
 }
--- wine-cellar-service/src/main/java/cz/muni/fi/pa165/facade/WineListFacadeImpl.java
+++ wine-cellar-service/src/main/java/cz/muni/fi/pa165/facade/WineListFacadeImpl.java
@@ -1,7 +1,78 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.MarketingEventDto;
+import cz.muni.fi.pa165.dto.WineListDto;
+import org.joda.time.LocalDateTime;
+
+import java.util.List;
+
 /**
  * @author Tomas Gordian on 11/6/2016.
  */
-public class WineListFacadeImpl {
+public class WineListFacadeImpl implements WineListFacade {
+
+    @Override
+    public void create(WineListDto wineList) {
+
+    }
+
+    @Override
+    public void delete(Long wineListId) {
+
+    }
+
+    @Override
+    public void update(Long wineListId) {
+
+    }
+
+    @Override
+    public void updateName(Long wineListId, String name) {
+
+    }
+
+    @Override
+    public void updateDate(Long wineListId, LocalDateTime date) {
+
+    }
+
+    @Override
+    public void updateMarketingEvent(Long wineListId, MarketingEventDto marketingEventDto) {
+
+    }
+
+    @Override
+    public List<WineListDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public WineListDto get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<WineListDto> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<WineListDto> findByDate(LocalDateTime date) {
+        return null;
+    }
+
+    @Override
+    public List<WineListDto> findByMarketingEvent(MarketingEventDto marketingEventDto) {
+        return null;
+    }
+
+    @Override
+    public WineListDto addWine(Long wineListId, Long wineId) {
+        return null;
+    }
+
+    @Override
+    public WineListDto removeWine(Long wineListId, Long wineId) {
+        return null;
+    }
 }
