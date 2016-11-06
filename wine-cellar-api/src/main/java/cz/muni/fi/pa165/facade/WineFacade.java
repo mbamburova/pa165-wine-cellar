6251486bc5762dedfe4804eab1653f09555d20b2
marekscholtz <marek.scholtz@gmail.com>
2016-11-06 21:35:35 +0100

    PackingService added

--- wine-cellar-api/src/main/java/cz/muni/fi/pa165/facade/WineFacade.java
+++ wine-cellar-api/src/main/java/cz/muni/fi/pa165/facade/WineFacade.java
@@ -1,11 +1,45 @@
 package cz.muni.fi.pa165.facade;
 
+import java.math.BigDecimal;
+import java.time.Year;
+
 /**
  * @author MarekScholtz
  * @version 2016.11.06
  */
 public interface WineFacade {
 
+    void create(WineDto wine);
+    void delete(Long wineId);
+    void update(Long wineId);
+    void updateName(Long wineId, String name);
+    void updateVintage(Long wineId, Year vintage);
+    void updateBatch(Long wineId, String batch);
+    void updatePredicate(Long wineId, String predicate);
+    void updatePredicateEquivalent(Long wineId, String predicateEquivalent);
+    void updateDescription(Long wineId, String description);
+    void updateNotes(Long wineId, String notes);
+    void updateAlcoholVolume(Long wineId, BigDecimal alcoholVolume);
+    void updateResidualSugar(Long wineId, BigDecimal residualSugar);
+    void updateAcidity(Long wineId, BigDecimal acidity);
+    void updateGrapeSugarContent(Long wineId, BigDecimal grapeSugarContent);
 
+    List<WineDto> findAll();
+    WineDto get(Long id);
+    List<WineDto> findByName(String name);
+    List<WineDto> findByVintage(Year vintage);
+    WineDto findByBatch(String batch);
+    List<WineDto> findByPredicate(String predicate);
+    List<WineDto> findByPredicateEquivalent(String predicateEquivalent);
+    List<WineDto> findByDescription(String description);
+    List<WineDto> findByNotes(String notes);
+    List<WineDto> findByAlcoholVolume(BigDecimal alcoholVolume);
+    List<WineDto> findByResidualSugar(BigDecimal residualSugar);
+    List<WineDto> findByAcidity(BigDecimal acidity);
+    List<WineDto> findByGrapeSugarContent(BigDecimal grapeSugarContent);
+    List<WineDto> findBetweenYears(Year from, Year to);
+
+    void addPackage(Long wine, Long packing);
+    void removePackage(Long wine, Long packing);
 
 }
