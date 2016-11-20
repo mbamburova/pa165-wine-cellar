85c99348abbdec310b6c2bbf5ee60d2595a1b5de
Michaela Bamburová <433732@mail.muni.cz>
2016-11-20 14:55:10 +0100

    More tests for Wine service.

--- wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/WineServiceTest.java
+++ wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/WineServiceTest.java
@@ -3,7 +3,7 @@ package cz.muni.fi.pa165.service;
 import cz.muni.fi.pa165.config.ServiceConfiguration;
 import cz.muni.fi.pa165.dao.WineDao;
 import cz.muni.fi.pa165.entity.Wine;
-import org.junit.After;
+import cz.muni.fi.pa165.exceptions.WineCellarServiceException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -19,11 +19,13 @@ import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * @author Michaela Bamburová on 08.11.2016.
+ * @author Michaela Bamburová on 08.11.2016
  */
 @ContextConfiguration(classes = ServiceConfiguration.class)
 public class WineServiceTest extends AbstractTestNGSpringContextTests {
@@ -102,94 +104,177 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
-    public void testCreateWine(){
+    public void testCreateWine() {
+        wineService.create(veltlinskeZelene);
+        verify(wineDao).createWine(veltlinskeZelene);
+    }
 
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNullName() {
+        veltlinskeZelene.setName(null);
+        doThrow(new WineCellarServiceException("Cannot create wine with null name."))
+            .when(wineDao)
+            .createWine(veltlinskeZelene);
+
+        wineService.create(veltlinskeZelene);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNullVintage() {
+        muskatMoravsky.setVintage(null);
+        doThrow(new WineCellarServiceException("Cannot create wine with null vintage."))
+            .when(wineDao)
+            .createWine(muskatMoravsky);
+
+        wineService.create(muskatMoravsky);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNullBatch() {
+        svatovavrinecke.setBatch(null);
+        doThrow(new WineCellarServiceException("Cannot create wine with null batch"))
+            .when(wineDao)
+            .createWine(svatovavrinecke);
+
+        wineService.create(svatovavrinecke);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNegativeAlcoholVolume() {
+        veltlinskeZelene.setAlcoholVolume(new BigDecimal("-0.6"));
+        doThrow(new WineCellarServiceException("Cannot create wine with negative alcohol volume"))
+            .when(wineDao)
+            .createWine(veltlinskeZelene);
+
+        wineService.create(veltlinskeZelene);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNegativeResidualSugar() {
+        muskatMoravsky.setResidualSugar(new BigDecimal("-0.1"));
+        doThrow(new WineCellarServiceException("Cannot create wine with negative residual sugar"))
+            .when(wineDao)
+            .createWine(muskatMoravsky);
+
+        wineService.create(muskatMoravsky);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNegativeAcidity() {
+        svatovavrinecke.setAcidity(new BigDecimal("-0.1"));
+        doThrow(new WineCellarServiceException("Cannot create wine with negative acidity."))
+            .when(wineDao)
+            .createWine(svatovavrinecke);
+
+        wineService.create(svatovavrinecke);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testCreateWineWithNegativeGrapeSugarContent() {
+        veltlinskeZelene.setGrapeSugarContent(new BigDecimal("-0.7"));
+        doThrow(new WineCellarServiceException("Cannot create wine with negative grape sugar content."))
+            .when(wineDao)
+            .createWine(veltlinskeZelene);
+
+        wineService.create(veltlinskeZelene);
     }
 
     @Test
-    public void testCreateWineWithNullName(){
+    public void testUpdateWine() {
+        wineService.update(muskatMoravsky);
+        verify(wineDao).updateWine(muskatMoravsky);
+    }
 
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNullName() {
+        veltlinskeZelene.setName(null);
+        doThrow(new WineCellarServiceException("Cannot update wine with null name."))
+            .when(wineDao)
+            .updateWine(veltlinskeZelene);
+
+        wineService.update(veltlinskeZelene);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNullVintage() {
+        muskatMoravsky.setVintage(null);
+        doThrow(new WineCellarServiceException("Cannot update wine with null vintage."))
+            .when(wineDao)
+            .updateWine(muskatMoravsky);
+
+        wineService.update(muskatMoravsky);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNullBatch() {
+        svatovavrinecke.setBatch(null);
+        doThrow(new WineCellarServiceException("Cannot update wine with null batch"))
+            .when(wineDao)
+            .updateWine(svatovavrinecke);
+
+        wineService.update(svatovavrinecke);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNegativeAlcoholVolume() {
+        veltlinskeZelene.setAlcoholVolume(new BigDecimal("-0.6"));
+        doThrow(new WineCellarServiceException("Cannot update wine with negative alcohol volume"))
+            .when(wineDao)
+            .updateWine(veltlinskeZelene);
+
+        wineService.update(veltlinskeZelene);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNegativeResidualSugar() {
+        muskatMoravsky.setResidualSugar(new BigDecimal("-0.1"));
+        doThrow(new WineCellarServiceException("Cannot update wine with negative residual sugar"))
+            .when(wineDao)
+            .updateWine(muskatMoravsky);
+
+        wineService.update(muskatMoravsky);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNegativeAcidity() {
+        svatovavrinecke.setAcidity(new BigDecimal("-0.1"));
+        doThrow(new WineCellarServiceException("Cannot update wine with negative acidity."))
+            .when(wineDao)
+            .updateWine(svatovavrinecke);
+
+        wineService.update(svatovavrinecke);
+    }
+
+    @Test(expectedExceptions = WineCellarServiceException.class)
+    public void testUpdateWineWithNegativeGrapeSugarContent() {
+        veltlinskeZelene.setGrapeSugarContent(new BigDecimal("-0.7"));
+        doThrow(new WineCellarServiceException("Cannot update wine with negative grape sugar content."))
+            .when(wineDao)
+            .updateWine(veltlinskeZelene);
+
+        wineService.update(veltlinskeZelene);
     }
 
     @Test
-    public void testCreateWineWithNullVintage(){
+    public void testFindWineById() {
+        when(wineDao.findWineById(veltlinskeZelene.getId()))
+            .thenReturn(veltlinskeZelene);
 
-    }
+        assertThat(wineService.get(veltlinskeZelene.getId()))
+            .isEqualToComparingFieldByField(veltlinskeZelene);
 
-    @Test
-    public void testCreateWineWithNullBatch(){
-
-    }
-
-    @Test
-    public void testCreateWineWithNegativeAlcoholVolume(){
-
-    }
-
-    @Test
-    public void testCreateWineWithNegativeResidualSugar(){
-
-    }
-
-    @Test
-    public void testCreateWineWithNegativeAcidity(){
-
-    }
-
-    @Test
-    public void testCreateWineWithNegativeGrapeSugarContent(){
-
-    }
-
-    @Test
-    public void testUpdateWine(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNullName(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNullVintage(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNullBatch(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNegativeAlcoholVolume(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNegativeResidualSugar(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNegativeAcidity(){
-
-    }
-
-    @Test
-    public void testUpdateWineWithNegativeGrapeSugarContent(){
-
-    }
-
-    @Test
-    public void testFindWineById(){
-
+        verify(wineDao).findWineById(veltlinskeZelene.getId());
     }
 
 
     @Test
-    public void testFindWineByNonExistingId(){
+    public void testFindWineByNonExistingId() {
+        when(wineDao.findWineById(-1L))
+            .thenReturn(null);
 
+        assertThat(wineService.get(-1L)).isNull();
+
+        verify(wineDao).findWineById(-1L);
     }
 
     @Test
@@ -206,27 +291,36 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
         for(int i = 0; i < expectedWines.size(); i++) {
             assertThat(actWines.get(i)).isEqualToComparingFieldByField(expectedWines.get(i));
         }
+
+        verify(wineDao).findAllWines();
     }
 
     @Test
     public void testFindWinesByName() {
+        String name = "Veltlínske zelené";
+
         List<Wine> expectedWines = new ArrayList<>();
         expectedWines.add(veltlinskeZelene);
 
-        when(wineDao.findByName("Veltlínske zelené")).thenReturn(expectedWines);
-        assertThat(wineService.findByName("Veltlínske zelené").size()).isEqualTo(expectedWines.size());
+        when(wineDao.findByName(name)).thenReturn(expectedWines);
+        assertThat(wineService.findByName(name).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByName(name);
     }
 
     @Test
     public void testFindWinesByVintage() {
+        Year year = Year.of(2015);
+
         List<Wine> expectedWines = new ArrayList<>();
+
         expectedWines.add(muskatMoravsky);
         expectedWines.add(svatovavrinecke);
 
-        Year year = Year.of(2015);
-
         when(wineDao.findByVintage(year)).thenReturn(expectedWines);
         assertThat(wineService.findByVintage(year).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByVintage(year);
     }
 
     @Test
@@ -234,18 +328,23 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
         String batch = "6/15";
 
         when(wineDao.findByBatch(batch)).thenReturn(svatovavrinecke);
-        assertThat(wineService.findByBatch(batch)).isEqualTo(svatovavrinecke);
+        assertThat(wineService.findByBatch(batch)).isEqualToComparingFieldByField(svatovavrinecke);
+
+        verify(wineDao).findByBatch(batch);
     }
 
     @Test
     public void testFindWinesByPredicate() {
         String predicate = "kabinetní víno";
+
         List<Wine> expectedWines = new ArrayList<>();
         expectedWines.add(veltlinskeZelene);
         expectedWines.add(muskatMoravsky);
 
         when(wineDao.findByPredicate(predicate)).thenReturn(expectedWines);
         assertThat(wineService.findByPredicate(predicate).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByPredicate(predicate);
     }
 
     @Test
@@ -257,8 +356,10 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
         expectedWines.add(muskatMoravsky);
         expectedWines.add(svatovavrinecke);
 
-        when(wineDao.findByPredicate(predicateEquivalent)).thenReturn(expectedWines);
+        when(wineDao.findByPredicateEquivalent(predicateEquivalent)).thenReturn(expectedWines);
         assertThat(wineService.findByPredicateEquivalent(predicateEquivalent).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByPredicateEquivalent(predicateEquivalent);
     }
 
     @Test
@@ -271,6 +372,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByDescription(description)).thenReturn(expectedWines);
         assertThat(wineService.findByDescription(description).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByDescription(description);
     }
 
     @Test
@@ -282,6 +385,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByNotes(notes)).thenReturn(expectedWines);
         assertThat(wineService.findByNotes(notes).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByNotes(notes);
     }
 
     @Test
@@ -294,6 +399,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByAlcoholVolume(alcoholVolume)).thenReturn(expectedWines);
         assertThat(wineService.findByAlcoholVolume(alcoholVolume).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByAlcoholVolume(alcoholVolume);
     }
 
     @Test
@@ -305,6 +412,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByResidualSugar(residualSugar)).thenReturn(expectedWines);
         assertThat(wineService.findByResidualSugar(residualSugar).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByResidualSugar(residualSugar);
     }
 
     @Test
@@ -316,6 +425,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByAcidity(acidity)).thenReturn(expectedWines);
         assertThat(wineService.findByAcidity(acidity).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByAcidity(acidity);
     }
 
     @Test
@@ -329,6 +440,8 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
         when(wineDao.findByGrapeSugarContent(grapeSugarContent)).thenReturn(expectedWines);
         assertThat(wineService.findByGrapeSugarContent(grapeSugarContent).size()).isEqualTo(expectedWines.size());
+
+        verify(wineDao).findByGrapeSugarContent(grapeSugarContent);
     }
 
     @Test
@@ -343,16 +456,7 @@ public class WineServiceTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testDeleteWine() {
-
-    }
-
-    @Test
-    public void testDeleteNonExistingWine() {
-
-    }
-
-    @Test
-    public void testWineIsValid() {
-
+        wineService.delete(veltlinskeZelene);
+        verify(wineDao).deleteWine(veltlinskeZelene);
     }
 }
