54fe1524676dc3f6ce642a671249d78af94f9495
Silvia Borzová <borzovasilvia@gmail.com>
2016-11-25 15:59:38 +0100

    packingservicetest changed

--- wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/PackingServiceTest.java
+++ wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/PackingServiceTest.java
@@ -6,6 +6,7 @@ import java.time.Year;
 import cz.muni.fi.pa165.WineBuilder;
 import cz.muni.fi.pa165.config.ServiceConfiguration;
 import cz.muni.fi.pa165.dao.PackingDao;
+import cz.muni.fi.pa165.dto.WineDto;
 import cz.muni.fi.pa165.entity.Packing;
 import cz.muni.fi.pa165.entity.Wine;
 import org.joda.time.LocalDateTime;
@@ -20,6 +21,9 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 /**
  * @author Silvia Borzová
  *         13/11/2016
@@ -31,6 +35,9 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
     @Mock
     private PackingDao packingDao;
 
+    @Mock
+    private WineDto wineDao;
+
     @Autowired
     @InjectMocks
     private PackingService packingService;
@@ -40,20 +47,42 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
 
     private Packing packing1;
     private Packing packing2;
+    private Packing packing3;
 
-    private WineBuilder wine() {
+    private Wine veltlinskeZelene;
+    private Wine muskatMoravsky;
+
+    private WineBuilder veltlinskeZelene() {
         return new WineBuilder()
                 .name("Veltlínske zelené")
                 .vintage(Year.of(2014))
                 .batch("10/14")
                 .predicate("kabinetní víno")
                 .predicateEquivalent("suché")
-                .description("Elegantní, svěží víno s lehkou aromatikou angreštu a zeleného pepře. Chuťový vjem je tvořen pikantní kyselinkou a kořenito-ovocnými tóny.")
+                .description("Elegantní, svěží víno s lehkou aromatikou angreštu a zeleného pepře. " +
+                        "Chuťový vjem je tvořen pikantní kyselinkou a kořenito-ovocnými tóny.")
                 .notes("20,0°ČNM")
-                .alcoholVolume(new BigDecimal(10.94))
-                .residualSugar(new BigDecimal(2.8))
-                .acidity(new BigDecimal(7.5))
-                .grapeSugarContent(new BigDecimal(0));
+                .alcoholVolume(new BigDecimal("10.94"))
+                .residualSugar(new BigDecimal("2.8"))
+                .acidity(new BigDecimal("7.5"))
+                .grapeSugarContent(new BigDecimal("0"));
+    }
+
+    private WineBuilder muskatMoravsky() {
+        return new WineBuilder()
+                .name("Muškát moravský")
+                .vintage(Year.of(2015))
+                .batch("1/14")
+                .predicate("kabinetní víno")
+                .predicateEquivalent("suché")
+                .description("Víno zlatavé barvy s ovocnou vůní citrusových plodů a muškátového oříšku." +
+                        " V chuti nabízí ovocné tóny grapefruitu a zralého citrónu. Ovocnou chuť provází příjemná kyselinka," +
+                        " díky níž je víno pikantní se suchým závěrem.")
+                .notes("20,2°ČNM")
+                .alcoholVolume(new BigDecimal("12"))
+                .residualSugar(new BigDecimal("0.7"))
+                .acidity(new BigDecimal("6.1"))
+                .grapeSugarContent(new BigDecimal("0"));
     }
 
     @BeforeClass
@@ -63,15 +92,31 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
 
     @BeforeMethod
     public void setUp() {
-        Wine wine = wine().build();
-        wineService.createWine(wine);
+        veltlinskeZelene = veltlinskeZelene().build();
+        muskatMoravsky = muskatMoravsky().build();
+        wineService.createWine(veltlinskeZelene);
+        wineService.createWine(muskatMoravsky);
 
         packing1 = new Packing();
         packing1.setVolume(new BigDecimal("0.75"));
-        packing1.setWine(wine);
+        packing1.setWine(veltlinskeZelene);
         packing1.setValidFrom(new LocalDateTime(2016,10,10,0,0));
         packing1.setValidTo(null);
         packingService.createPacking(packing1);
+
+        packing2 = new Packing();
+        packing2.setVolume(new BigDecimal("0.75"));
+        packing2.setWine(veltlinskeZelene);
+        packing2.setValidFrom(new LocalDateTime(2014,8,5,0,0));
+        packing2.setValidTo(new LocalDateTime(2015,12,11,0,0));
+        packingService.createPacking(packing2);
+
+        packing3 = new Packing();
+        packing3.setVolume(new BigDecimal("0.5"));
+        packing3.setWine(muskatMoravsky);
+        packing3.setValidFrom(new LocalDateTime(2016,10,10,0,0));
+        packing3.setValidTo(new LocalDateTime(2017,1,1,0,0));
+        packingService.createPacking(packing3);
     }
 
     @After
@@ -81,7 +126,14 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void createPacking(){
+        Packing packing = new Packing();
+        packing.setWine(veltlinskeZelene);
+        packing.setVolume(new BigDecimal("0.5"));
+        packing.setValidFrom(new LocalDateTime(2010,11,2,0,0));
+        packing.setValidTo(new LocalDateTime(2017,5,2,0,0));
+        packingService.createPacking(packing);
 
+        verify(packingDao, times(1)).createPacking(packing);
     }
 
     @Test
@@ -90,7 +142,7 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
-    public void findAllpackings(){
+    public void findAllPackings(){
 
     }
 
@@ -100,17 +152,28 @@ public class PackingServiceTest extends AbstractTestNGSpringContextTests {
     }
 
     @Test
-    public void deletePacking() throws Exception {
+    public void deletePacking(){
+        packingService.deletePacking(packing1);
+        verify(packingDao, times(1)).deletePacking(packing1);
+    }
+
+    @Test
+    public void packingIsValid(){
 
     }
 
     @Test
-    public void packingIsValid() throws Exception {
+    public void addWine(){
 
     }
 
     @Test
-    public void addWine() throws Exception {
+    public void findPackingsByVolume(){
+
+    }
+
+    @Test
+    public void findPackingsByWine(){
 
     }
 }
