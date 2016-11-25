0384d1f344f0ce0ebaf25793421c82637305a44b
Tomáš Gordian <xgordian@fi.muni.cz>
2016-11-25 21:16:44 +0100

    facade test initialization

---
+++ wine-cellar-service/src/test/java/cz/muni/fi/pa165/facade/WineFacadeTest.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.config.ServiceConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Tomas Gordian on 11/25/2016.
+ */
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class WineFacadeTest {
+}
