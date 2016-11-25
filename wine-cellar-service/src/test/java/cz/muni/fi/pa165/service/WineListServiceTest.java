7ce80b6657e5297c815542280a288ea62233aa2f
Marek Scholtz <marek.scholtz@gmail.com>
2016-11-25 16:13:31 +0100

    tests added

--- wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/WineListServiceTest.java
+++ wine-cellar-service/src/test/java/cz/muni/fi/pa165/service/WineListServiceTest.java
@@ -2,8 +2,10 @@ package cz.muni.fi.pa165.service;
 
 import cz.muni.fi.pa165.WineBuilder;
 import cz.muni.fi.pa165.dao.WineListDao;
+import cz.muni.fi.pa165.entity.MarketingEvent;
 import cz.muni.fi.pa165.entity.Wine;
 import cz.muni.fi.pa165.entity.WineList;
+import org.joda.time.LocalDateTime;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -14,8 +16,13 @@ import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author MarekScholtz
@@ -23,17 +30,20 @@ import static org.mockito.Mockito.verify;
  */
 public class WineListServiceTest extends AbstractTestNGSpringContextTests {
 
+    private WineList wineList1;
+    private WineList wineList2;
+    private Wine veltlinskeZelene;
+    private Wine muskatMoravsky;
+    private Wine svatovavrinecke;
+    
     @Mock
-    private WineListDao WineListDao;
+    private WineListDao wineListDao;
 
     @InjectMocks
     private WineService wineService;
 
     @InjectMocks
-    private WineListService WineListService;
-
-    private Wine veltlinskeZelene;
-    private Wine muskatMoravsky;
+    private WineListService wineListService;
 
     private WineBuilder veltlinskeZelene() {
         return new WineBuilder()
@@ -64,6 +74,24 @@ public class WineListServiceTest extends AbstractTestNGSpringContextTests {
                 .acidity(new BigDecimal(6.1))
                 .grapeSugarContent(new BigDecimal(0));
     }
+    
+    private WineBuilder svatovavrinecke() {
+        return new WineBuilder()
+                .name("Svatovavřinecké")
+                .vintage(Year.of(2015))
+                .batch("6/15")
+                .predicate("pozdní sběr")
+                .predicateEquivalent("suché")
+                .description("Jiskrné víno rubínových odstínů barvy. Kořenitá vůně višní a třešňové kůry. " +
+                        "Zabalená v nádechu kouře z dubového dřeva. Chuť charakterní pevná, v níž se snoubí tóny višní," +
+                        " svěží kyselinky a příjemného třísla.")
+                .notes("30,2°ČNM")
+                .alcoholVolume(new BigDecimal("12"))
+                .residualSugar(new BigDecimal("6.2"))
+                .acidity(new BigDecimal("4.6"))
+                .grapeSugarContent(new BigDecimal("0"));
+    }
+    
 
     @BeforeClass
     public void setUpMock(){
@@ -72,31 +100,62 @@ public class WineListServiceTest extends AbstractTestNGSpringContextTests {
 
     @BeforeMethod
     public void setUp() {
-        veltlinskeZelene = veltlinskeZelene().build();
-        muskatMoravsky = muskatMoravsky().build();
+        List<Wine> wines1 = new ArrayList<>();
+        wines1.add(veltlinskeZelene().build());
+        wines1.add(muskatMoravsky().build());
+        wineList1.setDate(LocalDateTime.now());
+        wineList1.setName("anniversary");
+        wineList1.setWines(wines1);
+        List<Wine> wines2 = new ArrayList<>();
+        wines1.add(muskatMoravsky().build());
+        wines1.add(svatovavrinecke().build());
+        wineList2.setDate(LocalDateTime.now());
+        wineList2.setName("birthday");
+        wineList2.setWines(wines2);
+        
     }
 
     @Test
     public void createWineList() throws Exception {
+        wineListService.createWineList(wineList1);
+        verify(wineListDao).createWineList(wineList1);
     }
 
     @Test
     public void updateWineList() throws Exception {
-
+        wineListService.createWineList(wineList1);
+        wineList1.setName("birthday");
+        wineListService.updateWineList(wineList1);
+        verify(wineListDao).updateWineList(wineList1);
     }
 
     @Test
     public void findWineListById() throws Exception {
-
+        when(wineListDao.findWineListById(wineList1.getId()))
+                .thenReturn(wineList1);
+        assertThat(wineListService.findWineListById(wineList1.getId()))
+                .isEqualToComparingFieldByField(wineList1);
+        verify(wineListDao).findWineListById(wineList1.getId());
     }
 
     @Test
     public void deleteWineList() throws Exception {
-
+        wineListService.createWineList(wineList1);
+        wineListService.deleteWineList(wineList1);
+        verify(wineListDao).deleteWineList(wineList1);
     }
 
     @Test
     public void findAllWineLists() throws Exception {
-
+        List<WineList> expectedWineLists = new ArrayList<>();
+        expectedWineLists.add(wineList1);
+        expectedWineLists.add(wineList2);
+        when(wineListDao.findAllWineLists()).thenReturn(expectedWineLists);
+        List<WineList> currentWineLists = wineListService.findAllWineLists();
+        assertThat(currentWineLists).isEqualTo(expectedWineLists.size());
+        for(int i = 0; i < expectedWineLists.size(); i++) {
+            assertThat(currentWineLists.get(i)).isEqualToComparingFieldByField(expectedWineLists.get(i));
+        }
+        verify(wineListDao).findAllWineLists();
     }
 }
