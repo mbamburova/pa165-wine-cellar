a9768bfcd7ab7b34d9e12f6fe884fe07053e3570
Michaela Bamburová <433732@mail.muni.cz>
2016-12-15 00:16:06 +0100

    Edited maven dependencies

--- wine-cellar-api/src/main/java/cz/muni/fi/pa165/dto/PackingCreateDto.java
+++ wine-cellar-api/src/main/java/cz/muni/fi/pa165/dto/PackingCreateDto.java
@@ -1,8 +1,7 @@
 package cz.muni.fi.pa165.dto;
 
-import com.sun.istack.internal.NotNull;
-
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
